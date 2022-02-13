fedc67546f887838d127c519261d8cba45983da4
dkgian <dkgian@gmx.de>
2022-02-13 09:29:53 +0100

    typo

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,8 @@
 <template>
 	<div class="about pa-5">
 		<h1>About us</h1>
-		<p>This is playground by vue, vuetify, ...</p>
+		<p>
+			This is playground for vue, vuetify, ...
+		</p>
 	</div>
 </template>
